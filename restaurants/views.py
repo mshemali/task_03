622f3ef7fef4010bbfa4db6e04dcb857bb3de0ff
mohd <meshemali@gmail.com>
2018-07-08 15:11:39 +0300

    task 03

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,36 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context ={ 
+        "my_list":[
 
-    }
+
+        {
+        
+            "restaurant_name":"a",
+            "food_type":"fast food"
+
+        },
+        {
+        
+            "restaurant_name":"b",
+            "food_type":"fast food"
+
+        },
+        {
+        
+            "restaurant_name":"c",
+            "food_type":"fast food"
+
+        }
+    ]}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
     context = {
-
-    }
+    "my_object" : {
+        "restaurant_name":"a",
+        "food_type":"fast food"
+    }}
     return render(request, 'detail.html', context)
